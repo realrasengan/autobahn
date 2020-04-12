cb1518733a02c3ccabdb293adf0b19433e13f406
Gabriel Sroka <gabrielsroka@gmail.com>
2020-04-12 15:40:09 -0700

    Update hn.py

--- hn.py
+++ hn.py
@@ -7,6 +7,5 @@ url = 'https://hacker-news.firebaseio.com/v0'
 ids = requests.get(f'{url}/topstories.json').json()
 for i, id in enumerate(ids[:total], 1):
     item = requests.get(f'{url}/item/{id}.json').json()
-    n = f'{i})'.ljust(3)
-    print(f"{n} {item['title']} | {item['url']}")
+    print(f"{i:2}) {item['title']} | {item['url']}")
     print(f"    https://news.ycombinator.com/item?id={id} | {item['by']} | {item['score']}")
