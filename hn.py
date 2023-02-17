9279ff1ebaeb671eb1afb8a1754746c0a349c787
Gabriel Sroka <gabrielsroka@gmail.com>
2023-02-16 20:27:46 -0800

    Update hn.py

--- hn.py
+++ hn.py
@@ -3,13 +3,7 @@
 import requests
 
 def link(url, name):
-    if url:
-        esc = '\033'
-        h = esc + ']8;;'
-        st = esc + '\\'
-        return h + url + st + name + h + st
-    else:
-        return name
+    return f'\x1b]8;;{url}\x1b\\{name}\x1b]8;;\x1b\\'
 
 total = 10
 url = 'https://hacker-news.firebaseio.com/v0'
@@ -18,5 +12,5 @@ with requests.Session() as session:
     ids = session.get(f'{url}/topstories.json').json()
     for i, id in enumerate(ids[:total], start=1):
         item = session.get(f'{url}/item/{id}.json').json()
-        print(f"{i:2}) {link(item.get('url'), item['title'])}")
-        print('    ' + link(f'https://news.ycombinator.com/item?id={id}', f"{item['by']} | {item['score']}"))
+        print(f"{i:2}) {link(item.get('url', ''), item['title'])}")
+        print('    ' + link(f"https://news.ycombinator.com/item?id={id}", f"{item['by']} | {item['score']}"))
