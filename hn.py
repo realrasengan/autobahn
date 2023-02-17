469e6718e6b30ca4777733882f902d8fbe205d64
Gabriel Sroka <gabrielsroka@gmail.com>
2023-02-16 18:07:07 -0800

    Update hn.py
    added escape sequence to show hyperlinks in terminal

--- hn.py
+++ hn.py
@@ -2,6 +2,15 @@
 
 import requests
 
+def link(url, name):
+    if url:
+        esc = '\033'
+        h = esc + ']8;;'
+        st = esc + '\\'
+        return h + url + st + name + h + st
+    else:
+        return name
+
 total = 10
 url = 'https://hacker-news.firebaseio.com/v0'
 
@@ -9,5 +18,5 @@ with requests.Session() as session:
     ids = session.get(f'{url}/topstories.json').json()
     for i, id in enumerate(ids[:total], start=1):
         item = session.get(f'{url}/item/{id}.json').json()
-        print(f"{i:2}) {item['title']} | {item.get('url')}")
-        print(f"    https://news.ycombinator.com/item?id={id} | {item['by']} | {item['score']}")
+        print(f"{i:2}) {link(item.get('url'), item['title'])}")
+        print('    ' + link(f'https://news.ycombinator.com/item?id={id}', f"{item['by']} | {item['score']}"))
